06037122bad0303b7ba9e12da6d3595d970a2878
Philippe Noel <philippe.noel@loria.fr>
2022-09-21 11:44:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# message-links.el
+
+Emacs library to add reference links in message-mode without html.
+
+## What it does
+
+Consider the following textual message with a long link:
+
+``` text
+Hello,
+
+blablabla ...
+... and the report is available in the following link: https://my-file-server.org/Y5yu9Z1+3K83NFyPRy0siDw583mc+4iEljFmkUdgIIJuE5OAZTyhV00rXSMu/LP6z/o8HoD8hnXVIg2bwkddXw==
+Blablabla ...
+
+Cheers
+
+```
+
+This could be transformed into:
+
+``` text
+Hello,
+
+blablabla ...
+... and the report is available in the link [1]
+Blablabla ...
+
+Cheers
+
+---links---
+[1] : https://my-file-server.org/Y5yu9Z1+3K83NFyPRy0siDw583mc+4iEljFmkUdgIIJuE5OAZTyhV00rXSMu/LP6z/o8HoD8hnXVIg2bwkddXw==
+```
+
+This mode allow the generation of the references in the text as follow:
+![message-links demo](doc/message-links-demo.gif)
+
+## How to use
+
+- Download this repository into `~/.emacs.d`
+``` emacs-lisp
+(load "~/.emacs.d/message-links.el/message-links.el")
+(require 'message-links-mode)
+(define-key message-mode-map (kbd "C-c l") 'message-links-add)
+```
+
+Then press `C-c l` when composing a message to add a link.
---
+++ message-links.el
@@ -0,0 +1,46 @@
+;;; message-links.el -*- lexical-binding: t; -*-
+
+(defvar message-links-link-header "\n\n---links---\n")
+
+(defun message-links-add-link (link)
+  "Insert the link into the under the correct part of the message defined by `message-links-link-header'"
+  (interactive "sLink to insert: ")
+  (save-excursion
+    (let ((short-link-index (number-to-string (1+ (message-links-get-max-short-link)))))
+      (insert (concat "[" short-link-index "]"))
+      (if (not (search-forward message-links-link-header nil t))
+          (progn ;; No message-links-link-header present in the message
+            (goto-char (point-max))
+            (insert message-links-link-header)
+            (insert (concat "[" short-link-index "] : " link)))
+        (progn ;; Message found in the compose message
+          (goto-char (point-max))
+          (insert (concat "\n[" short-link-index "] : " link)))))))
+
+(defun message-links-get-max-short-link ()
+  "Get the maximum index after `message-links-link-header'.
+
+Return the maximum value. Return 0 otherwise."
+  (let ((short-links '()))
+    (save-excursion
+      (goto-char (point-min))
+      (if (search-forward message-links-link-header nil t)  ; Move point at the end of the header link
+          (progn
+            (while (search-forward-regexp "[\\([0-9]\\)]" nil t)
+              (push (string-to-number (match-string-no-properties 1)) short-links))
+            (message "%s" short-links)
+            (apply #'max short-links))
+        0))))
+
+(defalias 'message-links-add 'message-links-add-link)
+
+;;;autoload
+(define-minor-mode message-links-mode
+  "Toggle message-links-mode
+
+Call `message-links-add-link' to add a link into you message buffer.
+"
+  :lighter " message-links")
+;;;###autoload
+
+(provide 'message-links-mode)
